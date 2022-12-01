365e52751067cd4ea61076197f1cb901aabe589d
zaynox555 <zayno4d@gmail.com>
2022-12-01 15:00:32 +0700

    okok

---
+++ android/app/src/main/kotlin/com/vigenesia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vigenesia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/Login.dart';
+
+// ignore_for_file: prefer_const_constructors
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    ));
